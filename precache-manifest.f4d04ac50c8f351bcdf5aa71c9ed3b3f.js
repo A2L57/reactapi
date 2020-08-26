53791b38c216e7d8eeb7de22ebc3af705c29ed39
Atul Pandey <imwithyou.7@gmail.com>
2020-08-26 19:08:34 +0530

    Updates

---
+++ precache-manifest.f4d04ac50c8f351bcdf5aa71c9ed3b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2c9af9cc4da48519c972ceafc5424c",
+    "url": "/reactapi/index.html"
+  },
+  {
+    "revision": "056416ae0892eda7fe2d",
+    "url": "/reactapi/static/css/main.71e9c00c.chunk.css"
+  },
+  {
+    "revision": "b715d71c2c77306805ff",
+    "url": "/reactapi/static/js/2.a738dfd3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapi/static/js/2.a738dfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056416ae0892eda7fe2d",
+    "url": "/reactapi/static/js/main.2e4ff4e1.chunk.js"
+  },
+  {
+    "revision": "4676561ec1163b38490a",
+    "url": "/reactapi/static/js/runtime-main.c9a29e95.js"
+  }
+]);
